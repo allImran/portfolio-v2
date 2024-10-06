f5922ffc2b7ff41457f612c73c86d301383c4fb3
allImran <badhan19@gmail.com>
2024-10-06 21:30:17 +0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+import sanity from '@sanity/astro'
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [sanity({
+      projectId: 'naj7xsf9',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      studioBasePath: '/admin'
+    }), react(), vue()],
+});
